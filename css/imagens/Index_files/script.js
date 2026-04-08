a1915d876be4f484150091e167ae196c5627af12
joao <jovitorabreu87@gmail.com>
2026-04-08 20:22:52 -0300

    Rename Index.html to index.html

---
+++ css/imagens/Index_files/script.js
@@ -0,0 +1,10 @@
+function tocarSom(){
+  document.getElementById("som").play();
+ 
+  
+}
+function tocarSom2(){
+
+document.getElementById("som2").play();
+
+}
---
+++ js/script.js
@@ -0,0 +1,13 @@
+function tocarSom(){
+  document.getElementById("som").play();
+ 
+  
+}
+function tocarSom2(){
+
+document.getElementById("som2").play();
+
+}
+function tocarSom3(){
+  document.getElementById("som3").play();
+}
